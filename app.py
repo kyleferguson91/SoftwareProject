70ecab4430db17ba7d688d50695ba6d5595dde88
Kyle Ferguson <kyleferguson91@gmail.com>
2025-01-20 17:11:50 -0500

    skeleton web app started

--- app.py
+++ app.py
@@ -1,16 +1,26 @@
+#venv\Scripts\activate
+#python app.py
 
-from flask import Flask
+
+from flask import Flask, render_template
+from livereload import Server
 
 app = Flask(__name__)
-
+app.debug = True
 
 @app.route('/')
 
-def hello_world():
-    return 'Hello World'
 
 
-if __name__ == '__main__':
+def mainpage():
+    return render_template('landing.html')
 
 
-    app.run()
+
+
+
+
+server = Server(app.wsgi_app)
+server.watch("templates/*.*") 
+server.watch("./static/css/*.*") 
+server.serve(port=5000)
