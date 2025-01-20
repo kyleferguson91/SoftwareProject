a17472c5f93adca2866509655f7585627c418a90
Kyle Ferguson <kyleferguson91@gmail.com>
2025-01-20 17:23:00 -0500

    added a basic form with post submission

--- app.py
+++ app.py
@@ -2,23 +2,23 @@
 #python app.py
 
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from livereload import Server
 
 app = Flask(__name__)
 app.debug = True
 
 @app.route('/')
-
-
-
 def mainpage():
     return render_template('landing.html')
 
 
+@app.route('/submit', methods=['POST'])
 
-
-
+def submit():
+        name = request.form['name']
+        name1 = request.form['name1']
+        return "name is " + name + " " + name1
 
 server = Server(app.wsgi_app)
 server.watch("templates/*.*") 
